1a0dcf53cbd5163b3bd2e41828b8b239681a99c9
BrianDouglas <bddrew86@gmail.com>
2021-01-22 09:08:24 -0800

    Assignment Complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,19 +1,37 @@
 // from data.js
-var tableData = data;
 var tbody = d3.select("#ufo-table");
 
-tableData.forEach(sighting => {
-    var row = tbody.append("tr");
-    Object.entries(sighting).forEach(([key,value]) => {
-        var cell = row.append("td");
-        if (key === "state" || key === "country"){
-            cell.text(value.toUpperCase());
-        } else if (key === "city") {
-            cell.text(value.split(' ')
-            .map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
-            .join(' '));
-        } else {
-            cell.text(value)
-        };
+var filterBtn = d3.select("#filter-btn");
+var filterForm = d3.select("#filter-form")
+filterBtn.on("click", runEnter);
+filterForm.on("submit", runEnter)
+
+loadData(data);
+
+function runEnter() {
+    d3.event.preventDefault();
+    var inputElem = d3.select("#datetime");
+    var inputVal = inputElem.property("value");
+    console.log(inputVal);
+    inputElem.property("value", "");
+    loadData(data.filter(row => row.datetime === inputVal));
+};
+
+function loadData(toLoad) {
+    tbody.html("")
+    toLoad.forEach(sighting => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key,value]) => {
+            var cell = row.append("td");
+            if (key === "state" || key === "country"){
+                cell.text(value.toUpperCase());
+            } else if (key === "city") {
+                cell.text(value.split(' ')
+                .map(w => w[0].toUpperCase() + w.substr(1).toLowerCase())
+                .join(' '));
+            } else {
+                cell.text(value)
+            };
+        });
     });
-});
+};
